e6e389ec5e2c6268c60f0e45c460e7b35d7d37a6
jasonparker <jason.parker2@hcahealthcare.com>
2021-11-02 11:17:15 -0500

    More debugging

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-from server import application
+from flask import Flask, abort, redirect, request
+import os
 
-if name == "main":
-    application.run()
+application = Flask(__name__)
+
+
+@application.route('/ping')
+def home_page():
+    """Health check route to ensure app is running."""
+    return "pong", 201, {'Content-type':'text/html'}
+
+
+if __name__ == "__main__":
+    application.run(
+        threaded=True,
+        host=os.environ['HOST'],
+        port=int(os.environ['PORT']))
