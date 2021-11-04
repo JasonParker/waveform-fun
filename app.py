58b454445b63a03815e178a100f6a2180434f66f
jasonparker <jason.parker2@hcahealthcare.com>
2021-11-03 23:36:36 -0500

    Placeholders for training and scoring routes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, abort, redirect, request
 import os
 
@@ -8,6 +9,27 @@ application = Flask(__name__)
 def home_page():
     """Health check route to ensure app is running."""
     return "pong", 201, {'Content-type':'text/html'}
+
+
+@application.route('/train')
+def training_route():
+    """Route to initiate model training"""
+    ## TODO: Connect this with modeling pipeline code
+    ##       Maybe you want to use a thread or similar to enable
+    ##       sending a success response to the requester immediately
+    ##       rather than waiting for the entire training pipeline to run.
+    return f"Model training initiated at {datetime.utcnow()}"
+
+
+@application.route('/score')
+def scoring_route():
+    """Route to generate a prediction"""
+    ## TODO: Stand up a function on this route that will:
+    ##       1. Accept a JSON object of the prediction data
+    ##       2. Generate a prediction
+    ##       3. Respond with a JSON object including the original
+    ##          data and the prediction
+    return f"Model training initiated at {datetime.utcnow()}"
 
 
 if __name__ == "__main__":
