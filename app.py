08bb7121595cfd344f524e578b46925f3a336061
hannahgmarshall <hannahgmarshall@gmail.com>
2021-11-04 17:28:52 +0000

    add scoring for auto ml

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from flask import Flask, abort, redirect, request
 import os
 
+from src.utils.predict_tabular_data import predict_tabular_classification_sample
+
 application = Flask(__name__)
 
 
@@ -31,9 +33,39 @@ def scoring_route():
     ##          data and the prediction
     return f"Model scoring initiated at {datetime.utcnow()}"
 
+@application.route('/automl_score')
+def scoring_route_auto_ml():
+    """Route to generate a prediction"""
+    ## TODO: Stand up a function on this route that will:
+    ##       1. Accept a JSON object of the prediction data
+    ##       2. Generate a prediction
+    ##       3. Respond with a JSON object including the original
+    ##          data and the prediction
+    sys_BP = request.args.get('sysBP',type=float)
+    dias_BP = request.args.get('diasBP',type=float)
+    map_BP = (sys_BP + 2 * dias_BP) / 3
+    
+    pred_input = {'avg_sys':str(sys_BP),
+                  'avg_map':str(map_BP),
+                  'avg_dias':str(dias_BP),
+                  'end_window':0,
+                  'start_window':0}
+    
+    
+    predictions = predict_tabular_classification_sample(
+        project="741350817607",
+        endpoint_id="8317875832869617664",
+        location="us-central1",instances=pred_input)
+    
+    for prediction in predictions:
+        current_pred = dict(prediction)
+    #return f"Patient Hypotensive in 15 min prediction: {datetime.utcnow()}"
+    return f"Patient Hypotensive in 15 min prediction: {current_pred}"
+
 
 if __name__ == "__main__":
     application.run(
         threaded=True,
-        host='0.0.0.0',
-        port=8080)
+        host='0.0.0.1',
+        port=8080
+    )
---
+++ src/utils/predict_tabular_data.py
@@ -0,0 +1,40 @@
+from typing import Dict
+
+from google.cloud import aiplatform
+from google.protobuf import json_format
+from google.protobuf.struct_pb2 import Value
+
+
+def predict_tabular_classification_sample(
+    project: str,
+    endpoint_id: str,
+    instance_dict: Dict,
+    location: str = "us-central1",
+    api_endpoint: str = "us-central1-aiplatform.googleapis.com",
+):
+    # The AI Platform services require regional API endpoints.
+    client_options = {"api_endpoint": api_endpoint}
+    # Initialize client that will be used to create and send requests.
+    # This client only needs to be created once, and can be reused for multiple requests.
+    client = aiplatform.gapic.PredictionServiceClient(client_options=client_options)
+    # for more info on the instance schema, please use get_model_sample.py
+    # and look at the yaml found in instance_schema_uri
+    instance = json_format.ParseDict(instance_dict, Value())
+    instances = [instance]
+    parameters_dict = {}
+    parameters = json_format.ParseDict(parameters_dict, Value())
+    endpoint = client.endpoint_path(
+        project=project, location=location, endpoint=endpoint_id
+    )
+    response = client.predict(
+        endpoint=endpoint, instances=instances, parameters=parameters
+    )
+    print("response")
+    print(" deployed_model_id:", response.deployed_model_id)
+    # See gs://google-cloud-aiplatform/schema/predict/prediction/tables_classification.yaml for the format of the predictions.
+    predictions = response.predictions
+    for prediction in predictions:
+        print(" prediction:", dict(prediction))
+    return predictions
+    
+    
